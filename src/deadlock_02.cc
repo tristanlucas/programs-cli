334ab9fd97a2beb918a571926296f693c75a8a41
tristanlucas <geosystem.kst@gmail.com>
2024-06-15 00:07:39 +0630

    deadlock

---
+++ src/deadlock_02.cc
@@ -0,0 +1,36 @@
+// 11_deadlock_02
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <utility>
+
+std::mutex resource1;
+std::mutex resource2;
+
+void threadFunction1() {
+    std::lock(resource1, resource2); // Lock both mutexes without deadlock
+    std::lock_guard<std::mutex> lock1(resource1, std::adopt_lock);
+    std::this_thread::sleep_for(std::chrono::seconds(5)); // Simulating work
+    std::lock_guard<std::mutex> lock2(resource2, std::adopt_lock);
+
+    std::cout << "Thread 1 has both mutexes" << std::endl;
+}
+
+void threadFunction2() {
+    std::lock(resource1, resource2); // Lock both mutexes without deadlock
+    std::lock_guard<std::mutex> lock2(resource2, std::adopt_lock);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000)); // Simulating work
+    std::lock_guard<std::mutex> lock1(resource1, std::adopt_lock);
+
+    std::cout << "Thread 2 has both mutexes" << std::endl;
+}
+
+int main() {
+    std::thread t1(threadFunction1);
+    std::thread t2(threadFunction1);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
