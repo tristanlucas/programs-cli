7d065fb6cdaa46d345e01411107dda3ec2cc5c76
tristanlucas <geosystem.kst@gmail.com>
2024-06-26 11:23:07 +0630

    loaded threads

---
+++ src/racedCondition03.cc
@@ -0,0 +1,51 @@
+/*Passing data to a thread in this way is a clean and safe method as there is no danger of a data race
+ — at least when atomic data types such as integers, doubles, chars or Booleans are passed.
+*/
+#include <iostream>
+#include <thread>
+#include <future>
+
+class Vehicle
+{
+public:
+    //default constructor
+    Vehicle() : _id(0)
+    {
+        std::cout << "Vehicle #" << _id << " Default constructor called" << std::endl;
+    }
+
+    //initializing constructor
+    Vehicle(int id) : _id(id)
+    {
+        std::cout << "Vehicle #" << _id << " Initializing constructor called" << std::endl;
+    }
+
+    // setter and getter
+    void setID(int id) { _id = id; }
+    int getID() { return _id; }
+
+private:
+    int _id;
+};
+
+int main()
+{
+    // create instances of class Vehicle
+    Vehicle v0; // default constructor
+    Vehicle v1(1); // initializing constructor
+
+    // read and write name in different threads (which one of the above creates a data race?)
+    std::future<void> ftr = std::async([](Vehicle v) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // simulate work
+        v.setID(2);
+    }, v0);
+
+    v0.setID(3);
+    
+
+    ftr.wait();
+    std::cout << "Vehicle #" << v0.getID() << std::endl;
+    std::cout << "Vehicle #" << v1.getID() << std::endl;
+
+    return 0;
+}
