3439381a1a05175b45cd27dc28f6f1ddda2ce249
tristanlucas <geosystem.kst@gmail.com>
2024-07-07 21:05:30 +0630

    package_task

--- 04_future/09_package_task.cpp
+++ 04_future/09_package_task.cpp
@@ -1,17 +1,30 @@
-#include<iostream>
-#include<thread>
-#include<future>
+#include <iostream>
+#include <future>
+#include <thread>
+#include <chrono>
+#include <deque>
 
-using namespace std::
+using namespace std;
 
-std::queue<std::packaged_task>int()> > task_q;
+int factorial(int N){
+    int res = 1;
+    for (int i = N; i > 1 ; i--){
+        res *= i;
+    }
+    cout << "Result is = " << res << endl;
+    return res;
+}
+
+
+std::deque<std::packaged_task<int()> > task_q;
 std::mutex mu;
 std::condition_variable cond;
 
 void thread_1(){
     std::packaged_task<int()> t;
     {
-        std::lock_guard<std::mutex> locker(mu);
+        std::unique_lock<std::mutex> locker(mu);
+        cond.wait(locker, [](){return !task_q.empty();});
         t= std::move(task_q.front());
         task_q.pop_front();
     }
@@ -20,15 +33,20 @@ void thread_1(){
 
 int main(){
     std::thread t1(thread_1);
-    std::packaged_task<int()> t(std::bind(factorial,6));
+    std::packaged_task<int()> t(bind(factorial, 6));
     std::future<int> fu= t.get_future();
     {
-    std::lock_guard<std::mutex> locker(mu);
-    task_q.push_back(std::move(t));
-}
+        std::lock_guard<std::mutex> locker(mu);
+        task_q.push_back(std::move(t));
+    }
     cond.notify_one();
-    cout<<fu.get();
+    std::cout<<fu.get();
 
     t1.join();
     return 0;
 }
+
+// 3 ways to get a future
+// - promise :: get_future()
+// - package_task :: get_future()
+// - async () return future();
